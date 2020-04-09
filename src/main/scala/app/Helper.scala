1583f6b1b2d7a1b30d2a1cf14a3fa1a9d987ad68
Aman Gupta <gupta.am@husky.neu.edu>
2020-04-09 15:27:51 -0700

    Compute Nightly price for houses

--- src/main/scala/app/Helper.scala
+++ src/main/scala/app/Helper.scala
@@ -11,9 +11,22 @@ object Helper {
   }
 
   def injectIsWithinPopular(popAreas: Seq[Try[PopularArea]], listings: Seq[Try[Listing]]): Seq[Try[Listing]] = {
-    val mile = 2
+    val mile = 1.5
     listings.map(l => {
       for (lx <- l; (popArea, isBoolean, minDistance)  = lx.hasClosestPopularArea(mile, lx, popAreas)) yield lx.copy(isWithinPopular = isBoolean, minDistance = minDistance , closestPopularArea = popArea)
     })
+  }
+
+  def computeHouseNightlyPrice(priceString: String): Long = {
+    val base = 50000
+    val price = Try(priceString.toDouble/365).getOrElse(0.0)
+    if(0 until base contains price)  (price/2).toLong
+    else if(base until base*2 contains price)  (price/3).toLong
+    else if(base*2 until base*4 contains price)  (price/4).toLong
+    else if(base*4 until base*8 contains price) (price/5).toLong
+    else if(base*8 until base*16 contains price) (price/6).toLong
+    else if(base*16 until base*32 contains price) (price/10).toLong
+    else if(base*32 until base*64 contains price) (price/15).toLong
+    else (price/20).toLong
   }
 }
