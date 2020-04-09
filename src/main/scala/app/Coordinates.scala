2ae0e1ca88fd788cd8335ed208889c2ac8e3e3a9
Aman Gupta <gupta.am@husky.neu.edu>
2020-04-09 14:44:48 -0700

    Handling empty values for coordinates

--- src/main/scala/app/Coordinates.scala
+++ src/main/scala/app/Coordinates.scala
@@ -4,8 +4,8 @@ import scala.util.{Failure, Success, Try}
 
 object Coordinates{
   def parse(params: List[String]): Try[Coordinates] = params match {
-    case lat :: longitude :: Nil => Try(apply(lat.toDouble, longitude.toDouble))
-    case _ => Failure(new Exception(s"logic error in Coordinates: $params"))
+    case lat :: longitude :: Nil => Success(apply(Try(lat.toDouble).getOrElse(0), Try(longitude.toDouble).getOrElse(0)))
+    case _ => Failure(throw new Exception(s"Logic error in processing coordinates"))
   }
 }
 case class Coordinates(lat: Double, longitude: Double){
